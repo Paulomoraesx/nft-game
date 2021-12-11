d0d4e9325e9ce62e3958a481a050d019f6017c63
revenantreturns <64325762+revenantreturns@users.noreply.github.com>
2021-12-11 13:25:58 -0300

    add functions

--- nft-game/src/components/CardMkt.jsx
+++ nft-game/src/components/CardMkt.jsx
@@ -2,40 +2,53 @@ import React from 'react'
 import { Box, Container, Flex, Text } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import '../css/style.css'
+import '../css/chickens.css'
+
 
 const styles = {
-    cardVendaGalinha: {
-        width: "300px",
-        border: "1px solid #696969",
-        margin: "3px",
-    },
-    ajustarAoCentro: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center"
-    }
+	cardVendaGalinha: {
+		width: "300px",
+		border: "1px solid #696969",
+		margin: "3px",
+	},
+	ajustarAoCentro: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center"
+	}
+}
+
+function gender(g) {
+	switch (g) {
+		case 1:
+			g = "Galinha"
+			break;
+		case 2:
+			g = "Galo"
+			break;
+	}
+	return g
 }
 
 export default function CardMkt() {
-
-
-    return (
-        <>
-            <Container>
-                <Flex align="center"
-                    justify="center"
-                    direction="column"
-                    p={10}
-                    style={styles.cardVendaGalinha}>
-                    <div id="galinha-normal" />
-                    <Box p={1} mt={5} style={styles.ajustarAoCentro}>
-                        <Text>Galinha Normal</Text>
-                        <Button mt={5}>Comprar</Button>
-                    </Box>
-                </Flex>
-            </Container>
-        </>
-    )
+	return (
+		<>
+			<Container>
+				<Flex align="center"
+					justify="center"
+					direction="column"
+					p={10}
+					style={styles.cardVendaGalinha}>
+					<div id="galinha-normal" />
+					<Box p={1} mt={5} style={styles.ajustarAoCentro}>
+						<Text>Galinha Normal</Text>
+						<Text>{gender(1)}</Text>
+						<Button mt={5}>Comprar</Button>
+					</Box>
+				</Flex>
+			</Container>
+		</>
+	)
 }
 
--- nft-game/src/pages/NftsWallet.jsx
+++ nft-game/src/pages/NftsWallet.jsx
@@ -7,67 +7,109 @@ import axios from 'axios'
 import { BASE_URL } from '../Utils/requests'
 import { CONFIG } from '../Utils/config'
 import { Button } from '@chakra-ui/react'
+import '../css/chickens.css'
+
+
+function gender(g) {
+	switch (g) {
+		case 1:
+			g = "Galinha"
+			break;
+		case 2:
+			g = "Galo"
+			break;
+	}
+	return g
+}
+
+
+function rarity(r) {
+	switch (r) {
+		case 1:
+			r = "Common"
+			break;
+		case 2:
+			r = "Rare"
+			break;
+		case 3:
+			r = "Super Rare"
+			break;
+		case 4:
+			r = "Epic"
+			break;
+		case 5:
+			r = "Legendary"
+			break;
+	}
+	return r
+}
+
+function renderImage(a, b) {
+	return "gw" + a + "_" + b
+}
 
 const Nfts = ({ nfts }) => {
-    console.log(nfts)
-    return (
-        <Container display={"flex"} flexDirection={"column"} id={nfts.id}>
-            {nfts ? (
-                <Flex
-                    align="center"
-                    justify="center"
-                    direction="column" p={5} borderWidth="1px" borderRadius="lg">
-                    <h3>rarity: {nfts.rarity}</h3>
-                    <p> atk: {nfts.atk}</p>
-                    <p> res: {nfts.res}</p>
-                    <p> breed: {nfts.breed}</p>
-                </Flex>
-            ) : (
-                <p>Usuário não encontrado</p>
-            )}
-        </Container>
-    );
+	return (
+		<Container display={"flex"} flexDirection={"column"}>
+			{nfts ? (
+				<Flex
+					id={nfts.id}
+					align="center"
+					justify="center"
+					direction="column" p={5} borderWidth="1px" borderRadius="lg">
+					<p class={renderImage(nfts.breed, nfts.gender)}></p>
+					<h3>{rarity(nfts.rarity)}</h3>
+					<p> atk: {nfts.atk}</p>
+					<p> res: {nfts.res}</p>
+					<p> tipo: {gender(nfts.gender)}</p>
+					<p> breed: {nfts.breed}</p>
+				</Flex>
+			) : (
+				<p>Usuário não encontrado</p>
+			)}
+		</Container>
+	);
 };
 
 export default function NftsWallet() {
 
-    var [lista, setLista] = useState('')
+	var [lista, setLista] = useState('')
 
-    const testarLista = () => {
-        axios.get(`${BASE_URL}/owner/nft`, CONFIG)
-            .then(response => {
-                setLista(response.data)
-            })
-    }
+	const testarLista = () => {
+		axios.get(`${BASE_URL}/owner/nft`, CONFIG)
+			.then(response => {
+				setLista(response.data)
+			})
+	}
 
-    useEffect(() => {
-        testarLista()
-    }, [lista])
+	useEffect(() => {
+		testarLista()
+	}, [lista])
 
 
 
-    return (
-        <>
-            <NavMenu />
-            <Text fontSize="3xl" style={{ display: "flex", justifyContent: "center", padding: "10px" }}
-            >Your's NFTs</Text>
-            <Container p={2} maxW="container.xl" borderWidth="1px" borderRadius="lg">
-                <Grid templateColumns="repeat(3, 1fr)">
-                    <GridItem p={5} colSpan={1}>
-                    </GridItem>
-                    <GridItem colSpan={7}>
-                        <Container p={5} maxW='container.xl' borderWidth="1px" borderRadius="lg">
-                            <Flex /* style={styles.mainCard} */>
-                                <Container>
-                                    <div>
-                                        {Array.isArray(lista) && lista.map((nfts) => <Nfts nfts={nfts} key={nfts.id} />)}
-                                    </div>
-                                </Container>
-                            </Flex>
-                        </Container>
-                    </GridItem>
-                </Grid>
-            </Container>
-        </>
-    )
+	return (
+		<>
+			<NavMenu />
+			<Text fontSize="3xl" style={{ display: "flex", justifyContent: "center", padding: "10px" }}
+			>Your's NFTs</Text>
+			<Container p={2} maxW="container.xl" borderWidth="1px" borderRadius="lg">
+				<Grid templateColumns="repeat(3, 1fr)">
+					<GridItem p={5} colSpan={1}>
+					</GridItem>
+					<GridItem colSpan={7}>
+						<Container p={5} maxW='container.xl' borderWidth="1px" borderRadius="lg">
+							<Flex /* style={styles.mainCard} */>
+								<Container>
+									<div>
+										{Array.isArray(lista) && lista.map((nfts) => <Nfts nfts={nfts} key={nfts.id} />)}
+									</div>
+								</Container>
+							</Flex>
+						</Container>
+					</GridItem>
+				</Grid>
+			</Container>
+		</>
+	)
 }
